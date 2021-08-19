be728ce862a5265c46f4736d8a42723417e11f99
datmv02 <89186008+datmv02@users.noreply.github.com>
2021-08-19 17:14:34 +0700

    upadate remove.php

--- testphp1_v2/remove.php
+++ testphp1_v2/remove.php
@@ -6,15 +6,11 @@ $id = isset($_GET['id']) ? $_GET['id'] : -1;
 if ($id == -1) {
     header("location:index.php");
 } else {
-    if (isset($_POST['cancel'])) {
-        header("location:index.php");
-    }
-    if (isset($_POST['ok'])) {
 
         $queryGetId = "DELETE from cars where car_id=$id";
         $result = exeQuery($queryGetId, false);
         header("location:index.php");
       
-    }
+   
 }
 
